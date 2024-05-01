99e05dc77f363a371db7522a480d2ebd68e16469
Sartaz Ansari <24587635+SartazAnsari@users.noreply.github.com>
2024-05-01 23:56:10 +0530

    Adding project files

--- OverChargeAlert.py
+++ OverChargeAlert.py
@@ -3,14 +3,14 @@ import subprocess
 import sys
 
 # Path to the script directory
-script_dir = "D:\\Projects\\Python\\over-charge-alert"
+script_dir = 'D:\\Projects\\Python\\over-charge-alert'
 
 def run_script_in_virtualenv():
     # Path to the Python interpreter in the virtual environment
-    python_bin = os.path.join(script_dir, "over-charge-alert-env", "Scripts", "python.exe")
+    python_bin = os.path.join(script_dir, 'over-charge-alert-env', 'Scripts', 'python.exe')
     
     # Path to your Python script
-    script_file = os.path.join(script_dir, "OverChargeAlert.py")
+    script_file = os.path.join(script_dir, 'OverChargeAlert.py')
     
     # Run the script using the Python interpreter in the virtual environment
     subprocess.Popen([python_bin, script_file])
@@ -23,7 +23,7 @@ def is_already_running():
     return False
 
 # Check if we're running inside a virtual environment
-if hasattr(sys, "real_prefix") or (hasattr(sys, "base_prefix") and sys.base_prefix != sys.prefix):
+if hasattr(sys, 'real_prefix') or (hasattr(sys, 'base_prefix') and sys.base_prefix != sys.prefix):
     import psutil
     import time
     from infi.systray import SysTrayIcon
@@ -44,18 +44,18 @@ if hasattr(sys, "real_prefix") or (hasattr(sys, "base_prefix") and sys.base_pref
 
         if plugged:
             if percent == 100:
-                toaster.show_toast("Over Charge Alert", "Battery is fully \U0001F50B charged.\nPlease unplug \U0001F50C your charger.", duration=10)
+                toaster.show_toast('Over Charge Alert', 'Battery is fully \U0001F50B charged.\nPlease unplug \U0001F50C your charger.', duration=10)
             elif percent > alert:
-                toaster.show_toast("Over Charge Alert", "Battery charged over " + str(alert) + "% \u26A1.\nPlease unplug \U0001F50C your charger.", duration=10)
+                toaster.show_toast('Over Charge Alert', 'Battery charged over ' + str(alert) + '% \u26A1.\nPlease unplug \U0001F50C your charger.', duration=10)
             elif percent == alert:
-                toaster.show_toast("Over Charge Alert", "Battery charged to " + str(alert) + "% \u26A1.\nPlease unplug \U0001F50C your charger.", duration=10)
+                toaster.show_toast('Over Charge Alert', 'Battery charged to ' + str(alert) + '% \u26A1.\nPlease unplug \U0001F50C your charger.', duration=10)
             
 
     def exit_action(systray):
         os.kill(os.getpid(), signal.CTRL_C_EVENT)
 
-    menu_options = (("Check charge", None, check_battery),)
-    systray = SysTrayIcon("icon.ico", "Battery Monitor", menu_options, on_quit=exit_action)
+    menu_options = (('Check charge', None, check_battery),)
+    systray = SysTrayIcon('icon.ico', 'Battery Monitor', menu_options, on_quit=exit_action)
     systray.start()
 
     while True:
